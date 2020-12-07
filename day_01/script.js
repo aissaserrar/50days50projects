e1c122459f19cc00a1bf37dbb6359fb1514e18d1
unknown <aissa.serrar95@gmail.com>
2020-12-07 20:07:38 +0100

    DAY 01

--- day_01/script.js
+++ day_01/script.js
@@ -50,3 +50,20 @@ images.forEach((image) => {
 })
 
 container.firstElementChild.classList.add('active')
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    if (panel.classList.contains('active')) {
+      panel.classList.remove('active')
+    }
+  })
+}
